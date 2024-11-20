61b82a06082820c1c5de4026da8a7915ea53007f
AbhishekJha1916 <akjha418@gmail.com>
2024-11-20 12:37:25 +0530

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Poppins } from '@next/font/google';
-
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
 
 const geistSans = localFont({
@@ -29,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={poppins.className}
-      >
+      <body>
         {children}
       </body>
     </html>
